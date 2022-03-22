402988353a73ed25d827fe76bd18495172c757f4
aizimpamvu <hallyiizza200@gmail.com>
2022-03-22 18:04:05 +0200

    Using tuples

--- main.py
+++ main.py
@@ -3,17 +3,31 @@ from turtle import Screen
 import random
 
 tim = t.Turtle()
-
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
-           "SeaGreen"]
-directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
+
+"""colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray",
+           "SeaGreen"]"""
+# Using list
+directions = [0, 90, 180, 270]
+
+# Using turple
+t.colormode(255)
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
 for _ in range(200):
-    tim.color(random.choice(colours))
+    # tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
-s=Screen()
+s = Screen()
 screen = Screen()
 s.exitonclick()
-
